bf9a1f775dc0eda648df3eaa725f4fffd8cbb5d8
Marco Sirabella <msirabel@gmail.com>
2016-03-31 18:41:23 -0400

    - Added error message when used not on a raspberry pi devices

--- src/gpiopi.py
+++ src/gpiopi.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import RPi.GPIO as GPIO
+import sys
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    print("Not on a Raspberry Pi, use antipi.py for testing purposes")
+    sys.exit(1)
 GPIO.setmode(GPIO.BCM)
-
-print("Raspberry Pi GPIO module Version {0}".format(GPIO.VERSION))a
 
 GPIO.setup(0, GPIO.OUT)
 GPIO.setup(2, GPIO.OUT)
@@ -15,6 +18,8 @@ GPIO.setup(5, GPIO.IN)
 GPIO.setup(22, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 GPIO.setup(25, GPIO.IN)
+
+print("Raspberry Pi GPIO module Version {0}".format(GPIO.VERSION))
 
 def leftWinOpen():
     pass
