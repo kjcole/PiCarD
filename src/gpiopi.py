7839ce0e70173a6db6e3f0765dd6dcb01d22b3f7
Marco Sirabella <msirabel@gmail.com>
2016-03-31 17:46:21 -0400

    - Added start code to gpiopi.py

--- src/gpiopi.py
+++ src/gpiopi.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def 
+print("Raspberry Pi GPIO module Version {0}".format(GPIO.VERSION))
+def leftWinOpen():
+    pass
+
+
+def leftWinClose():
+    pass
+
+
+def rightWinOpen():
+    pass
+
+
+def rightWinClose():
+    pass
+
+
+def sunRoofOpen():
+    pass
+
+
+def sunRoofClose():
+    pass
