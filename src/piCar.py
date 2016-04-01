94168ae5ffa04f2247aad3198e76f92a1904ac18
Marco Sirabella <msirabel@gmail.com>
2016-04-01 14:52:28 -0400

    - Added threading functionality

--- src/piCar.py
+++ src/piCar.py
@@ -7,6 +7,7 @@ import sys
 
 from antipi import * #Use antipi, if not run on the Raspberry Pi
 import gui as gui #Change first gui to whatever your gui file is named
+from thread import *
 
 class Dashboard(QDialog, gui.Ui_Dashboard):
     def __init__(self, parent=None):
@@ -15,13 +16,22 @@ class Dashboard(QDialog, gui.Ui_Dashboard):
         self.setupUi(self)
     
     def leftWinButton(self):
-        #Add function like this "x=(x+1)%2"
+        "Add function like this x=(x+1)%2"
         leftwin = open("./stat/leftwin", "r")
         status = leftwin.read()
         leftwin.close()
         if status == "o":
             leftWinClose()
-            #Code to grey out button goes here
+            "Code to grey out button goes here"
+            thread(leftWinMax())
+            "run following code when thread finishes"
+            "set button to closing instead of opening"
+        elif status == "c":
+            leftWinOpen()
+            "Code to grey out button goes here"
+            thread(leftWinMax())
+            "run following code when thread finishes"
+            "set button to opening instead of closing"
 
 
     def rightWinButton(self):
---
+++ src/thread.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from PySide.QtCore import *
+from PySide.Gui import *
+
+def thread(job):
+    workerThread = WorkerThread(len(self.workerThreads) + 1)
+    self.workerThreads.append(workerThread)    # Add new thread to queue
+    self.workerThreads[-1].talk.connect(self.threadDone)       # int
+    self.workerThreads[-1].talk[str].connect(self.threadDone)  # str
+    self.workerThreads[-1].start()             # Start the new thread
+    job
+
