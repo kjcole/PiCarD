01fe11006c32cb820f30892fe874b95a8b901278
Martin Ennis <mcmennis@gmail.com>
2016-04-07 14:53:38 -0400

    Merge branch 'master' of github.com:kjcole/PiCarD

--- src/piCar.py
+++ src/piCar.py
@@ -7,12 +7,38 @@ import sys
 
 from antipi import * #Use antipi, if not run on the Raspberry Pi
 import gui as gui #Change first gui to whatever your gui file is named
+from thread import *
 
 class Dashboard(QDialog, gui.Ui_Dashboard):
     def __init__(self, parent=None):
         """Construct a Dialog window and fill with widgets"""
         super(Dashboard, self).__init__(parent) 
         self.setupUi(self)
+    
+    def leftWinButton(self):
+        "Add function like this x=(x+1)%2"
+        leftwin = open("./stat/leftwin", "r")
+        status = leftwin.read()
+        leftwin.close()
+        if status == "o":
+            leftWinClose()
+            "Code to grey out button goes here"
+            thread(leftWinMax())
+            "run following code when thread finishes"
+            "set button to closing instead of opening"
+        elif status == "c":
+            leftWinOpen()
+            "Code to grey out button goes here"
+            thread(leftWinMax())
+            "run following code when thread finishes"
+            "set button to opening instead of closing"
+
+
+    def rightWinButton(self):
+        pass
+
+    def sunRoofButton(self):
+        pass
 
 def main():
     app = QApplication(sys.argv)
