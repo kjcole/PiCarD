bf380670e4ee1f64caff58c77dd26d643da9f087
Marco Sirabella <msirabel@gmail.com>
2016-04-07 16:50:17 -0400

    - Added extra communication between this and main program

--- src/thread.py
+++ src/thread.py
@@ -4,10 +4,11 @@
 from PySide.QtCore import *
 
 def thread(job):
-    workerThread = WorkerThread(len(self.workerThreads) + 1)
+    workerThread = WorkerThread(len(self.workerThreads) + 1, job)
     self.workerThreads.append(workerThread)    # Add new thread to queue
     self.workerThreads[-1].talk.connect(self.threadDone)       # int
     self.workerThreads[-1].talk[str].connect(self.threadDone)  # str
     self.workerThreads[-1].start()             # Start the new thread
-    job
+    print(job,"is running")
+
 
