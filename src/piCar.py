0a762069974caee18078c8c016133809d34bc4d4
Marco Sirabella <msirabel@gmail.com>
2016-04-07 15:12:24 -0400

    - Fixed thread being mistaken for python threading - Fixed button not being assigned action

--- src/piCar.py
+++ src/piCar.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 from PySide.QtCore import *
 from PySide.QtGui  import *
 import sys
 
 from antipi import * #Use antipi, if not run on the Raspberry Pi
 import gui as gui #Change first gui to whatever your gui file is named
-from thread import *
+import thread
 
 class Dashboard(QDialog, gui.Ui_Dashboard):
     def __init__(self, parent=None):
         """Construct a Dialog window and fill with widgets"""
-        super(Dashboard, self).__init__(parent) 
+        super(Dashboard, self).__init__(parent)
         self.setupUi(self)
-    
-    def leftWinButton(self):
+        self.leftWinButton.clicked.connect(self.leftWinAction)
+
+    def leftWinAction(self):
         "Add function like this x=(x+1)%2"
-        leftwin = open("./stat/leftwin", "r")
+        leftwin = open("./.stat/leftwin", "r")
         status = leftwin.read()
         leftwin.close()
         if status == "o":
+            print("left window is open")
             leftWinClose()
             "Code to grey out button goes here"
-            thread(leftWinMax())
+            thread.thread(leftWinMax())
             "run following code when thread finishes"
             "set button to closing instead of opening"
         elif status == "c":
