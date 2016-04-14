2cd3da852c4a019090d39a4a694bec543ce58405
Marco Sirabella <msirabel@gmail.com>
2016-04-14 14:30:49 -0400

    - Added input definition-- unfinished

--- src/spoof.py
+++ src/spoof.py
@@ -3,7 +3,11 @@
 
 import sys
 
-HIGH=1
-LOW=0
+HIGH="1"
+LOW="0"
 def output(pinnum, state):
-    exec("pin{0} = {1}".format(pinnum,state))
+    exec("pin{0} = {1}".format(pinnum,state), globals())
+    print("setting pin",pinnum,"to",state)
+def wait(pinnum)
+    input("press enter to simulate pin",pinnum,"being turned to high for 0.1 seconds then back to low")
+
