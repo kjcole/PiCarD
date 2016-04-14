021148d7bb29aa516aa180d528f3666613e5d124
Martin Ennis <mcmennis@gmail.com>
2016-04-14 18:02:44 -0400

    - Added new test GUI for all to use.

---
+++ src/testgui.py
@@ -0,0 +1,24 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+import sys 
+from gpiopi import * 
+
+def action():
+    sunRoofOpen()
+    QMessageBox.information(win, "Goodbye", "Go to Hellokittyland")
+ 
+def action2():   
+    QMessageBox.warning(win, "I'm warning you!", "Go away already") 
+
+app = QApplication(sys.argv)
+win = QDialog()
+button1 = QPushButton("Hello")
+button2 = QPushButton("Hell")
+layout = QVBoxLayout()
+layout.addWidget(button1)
+layout.addWidget(button2)
+win.setLayout(layout)
+button1.clicked.connect(action)
+button2.clicked.connect(action2)
+win.show()
+app.exec_() 
