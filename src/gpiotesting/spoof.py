4bcb2cf5074e88e6b5b785014505c3e97b888c16
Marco Sirabella <msirabel@gmail.com>
2016-05-06 14:17:05 -0400

    - Changed up some stuff temp only is able to setmode once all objects in definition are the same

--- src/gpiotesting/spoof.py
+++ src/gpiotesting/spoof.py
@@ -4,21 +4,47 @@
 import sys
 
 VERSION=-1.0
-HIGH="1"
-LOW="0"
+HIGH="HIGH"
+LOW="LOW"
 IN="IN"
 OUT="OUT"
 BCM = "BCM"
 BOARD = "BOARD"
+pinbcm = (None, None, None, 2, None, 3, None, 4, 14, None, 15,
+          17, 18, 27, None, 22, 23, None, 24, 10, None,
+          9, 25, 11, 8, None, 7, None, None, 5, None,
+          6, 12, 13, None, 19, 16, 26, 20, None, 21)
+
+pinboard = (None, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40)
+
+pins = []
+for pin in range(41):
+    pins.append(None)
+
+pins = []
+for pin in range(41):
+    pins.append({"state": None,
+                 "dir": None,
+                 "bcm": pinbcm[pin],
+                 "board": pin})
 
 def setmode(mode):
     pass
+
 def setup(pinnum, direction):
     pass
+
 def output(pinnum, state):
-#    pinnum = state
-    exec("pin{0} = {1}".format(pinnum,state), globals())
+    global pins
+#   pinnum = state
+#   exec("pin{0} = {1}".format(pinnum,state), globals())
+#   pins[pinnum] = state
+    pins[pinnum]["state"] = state
     print("setting pin",pinnum,"to",state)
+
 def wait(pinnum):
     input("press enter to simulate pin",pinnum,"being turned to high for 0.1 seconds then back to low")
 
--- src/gpiotesting/temp.py
+++ src/gpiotesting/temp.py
@@ -12,8 +12,8 @@ BOARD = "BOARD"
 mode = None
 
 class Pin(object):
-    def __init__(self, num):
-        self.num = num
+    def __init__(self):
+#        self.num = num
         self.state = None
         self.dir = None
 
@@ -22,28 +22,40 @@ pinbcm = (None, None, 2, None, 3, None, 4, 14, None, 15,
           9, 25, 11, 8, None, 7, None, None, 5, None,
           6, 12, 13, None, 19, 16, 26, 20, None, 21)
 
-pins = []
-for pin in range(40):
-    pins.append(Pin(None))
+
+pins = {}
+
+#pins = []
+#for pin in range(40):
+#    pins.append(Pin(None))
+
 def setmode(numsys):
     global mode, pins
     mode = numsys
     for pin in range(40):
         if mode == BCM:
-            pins[pin].num=pinbcm[pin-1]
+            pins[pinbcm[pin-1]] = Pin
         elif mode == BOARD:
-            pins[pin].num=pin
+            pins[pin+1] = Pin
         else:
+###### For some reason setmode will not work a second time even if everything is reset to defaults ######
             print("something's wrong")
     print("setting mode to", mode)
 
 def setup(pinnum, direction):
-    for pin in range(40):
-        if pins[pin].num == pinnum:
-            pins[pin].dir = direction
+#    for pin in range(40):
+#        if pins[pin].num == pinnum:
+#            pins[pin].dir = direction
+    pins[pinnum].dir = direction
 
 def output(pinnum, state):
-    for pin in range(40):
-        if pins[pin].num == pinnum:
-            pins[pin].state = state
-            print("setting {0} pin {1} to {2}")
+    if pins[pinnum].dir == OUT:
+#    for pin in range(40):
+#        if pins[pin].num == pinnum:
+#            pins[pin].state = state
+        pins[pinnum].state = state
+        print("setting {0} pin {1} to {2}")
+    elif pins[pinnum].dir == IN:
+        print("Make sure the pin is on output")
+    else:
+        print("404")
