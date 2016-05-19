9a58a2604923402c653e5eeb5a9edd996f2b5a45
Kevin Cole <dc.loco@gmail.com>
2016-05-19 18:30:31 -0400

    - Imported GPIO functions and initialized pins - Added "buzzer" action to "front" button (pushButton) - Added dummy "quiet" action to "rear" button (pushButton_2)

--- guidesign/testgui.py
+++ guidesign/testgui.py
@@ -9,6 +9,7 @@ from PySide.QtGui  import *
 import time
 import buttonsv1
 
+import RPi.GPIO as GPIO
 
 class MainDialog(QDialog, buttonsv1.Ui_Dialog):
     def __init__(self, parent=None):
@@ -18,10 +19,23 @@ class MainDialog(QDialog, buttonsv1.Ui_Dialog):
         self.showFullScreen()
 
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.buzz)
+        self.pushButton_2.clicked.connect(self.quiet)
 
+    def buzz (self):
+        while True:
+            GPIO.output(23, GPIO.HIGH)
+            time.sleep(0.0011)
+            GPIO.output(23, GPIO.LOW)
+            time.sleep(0.0011)
+
+    def quiet (self):
+        pass
 
 def main():
     import sys
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23, GPIO.OUT)
     app = QApplication(sys.argv)
     touchscreen = MainDialog()
     touchscreen.show()
