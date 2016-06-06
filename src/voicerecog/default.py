999eac33b0c110d7e37f29471fef5371346b2555
Marco Sirabella <msirabel@gmail.com>
2016-06-06 13:42:41 -0400

    - Working raw audio file to text

---
+++ src/voicerecog/default.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from os import environ, path
+
+from pocketsphinx.pocketsphinx import *
+from sphinxbase.sphinxbase import *
+
+MODELDIR = "model"
+DATADIR = "data"
+
+# Create a decoder with certain model
+config = Decoder.default_config()
+config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+decoder = Decoder(config)
+
+# Decode streaming data.
+decoder = Decoder(config)
+decoder.start_utt()
+stream = open(path.join(DATADIR, 'goforward.raw'), 'rb')
+while True:
+  buf = stream.read(1024)
+  if buf:
+    print("decoding in progress")
+    decoder.process_raw(buf, False, False)
+  else:
+    break
+    print('boop')
+decoder.end_utt()
+print ('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
+
