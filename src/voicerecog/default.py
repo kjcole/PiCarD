076a8e51d310734499d56f9ed8d66057e6d5d5ca
Kevin Cole <dc.loco@gmail.com>
2016-06-22 14:19:58 -0400

    - Changed single-quotes to double-quotes in default.py cuz I like 'em better - Cleaned up default.py PEP8 errors - Added command line options and user prompting for filename to default.py

--- src/voicerecog/default.py
+++ src/voicerecog/default.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
 from os import environ, path
 
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 
 MODELDIR = "model"
-DATADIR = "data"
+DATADIR  = "data"
 
 # Create a decoder with certain model
 config = Decoder.default_config()
-config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
-config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+config.set_string("-hmm",  path.join(MODELDIR, "en-us/en-us"))
+config.set_string("-lm",   path.join(MODELDIR, "en-us/en-us.lm.bin"))
+config.set_string("-dict", path.join(MODELDIR, "en-us/cmudict-en-us.dict"))
 decoder = Decoder(config)
 
 # Decode streaming data.
 decoder = Decoder(config)
 decoder.start_utt()
-stream = open(path.join(DATADIR, 'hellotest16.wav'), 'rb')
-while True:
-  buf = stream.read(1024)
-  if buf:
-    print("decoding in progress")
-    decoder.process_raw(buf, False, False)
-  else:
-    break
-    print('boop')
-decoder.end_utt()
-print ('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
 
+if len(sys.argv) > 1:
+    speech = sys.argv[1]
+else:
+    speech = input("\nSpeech file name [default.raw]: ")
+if speech == "":
+    speech = "default.raw"
+
+stream = open(path.join(DATADIR, speech), "rb")
+while True:
+    buf = stream.read(1024)
+    if buf:
+        print("decoding in progress")
+        decoder.process_raw(buf, False, False)
+    else:
+        print("boop")
+        break
+
+decoder.end_utt()
+print ("Best hypothesis segments: ", [seg.word for seg in decoder.seg()])
